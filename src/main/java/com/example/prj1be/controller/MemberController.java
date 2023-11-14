2f6f6e40f438cc624f16d6b3424c3a9901b3e677
Nighteast <jung946244@naver.com>
2023-11-14 09:51:39 +0900

    로그아웃 기능

--- src/main/java/com/example/prj1be/controller/MemberController.java
+++ src/main/java/com/example/prj1be/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.example.prj1be.controller;
 
 import com.example.prj1be.domain.Member;
 import com.example.prj1be.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -110,6 +111,14 @@ public class MemberController {
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    // 로그아웃 기능
+    @PostMapping("logout")
+    public void logout(HttpSession session) {
+        if (session != null) {
+            session.invalidate();
         }
     }
 }
