559d4622a55eb94afb6cce9394bcbbbb6cb8f452
Nighteast <jung946244@naver.com>
2023-11-14 09:44:44 +0900

    로그인 후 Member객체 session attribute "login" 추가

--- src/main/java/com/example/prj1be/service/MemberService.java
+++ src/main/java/com/example/prj1be/service/MemberService.java
@@ -4,6 +4,8 @@ import com.example.prj1be.domain.Member;
 import com.example.prj1be.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
@@ -79,11 +81,13 @@ public class MemberService {
 
 
     // 회원 로그인 서비스
-    public boolean login(Member member) {
+    public boolean login(Member member, WebRequest request) {
         Member dbMember = mapper.selectById(member.getId());
 
         if (dbMember != null) {
             if (dbMember.getPassword().equals(member.getPassword())) {
+                dbMember.setPassword("");
+                request.setAttribute("login", dbMember, RequestAttributes.SCOPE_SESSION);
                 return true;
             }
         }
