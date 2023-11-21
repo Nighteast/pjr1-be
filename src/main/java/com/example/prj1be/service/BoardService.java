99778293a0bea37912f274c06a35b7ab89324d0c
Nighteast <jung946244@naver.com>
2023-11-21 10:14:45 +0900

    첨부파일 있는 게시물 삭제

--- src/main/java/com/example/prj1be/service/BoardService.java
+++ src/main/java/com/example/prj1be/service/BoardService.java
@@ -155,6 +155,9 @@ public class BoardService {
         // 좋아요 레코드 지우기
         likeMapper.deleteByBoardId(id);
 
+        // 첨부파일 이미지 레코드 지우기
+        fileMapper.deleteByBoardId(id);
+
         return mapper.deleteById(id) == 1;
     }
 
